7a1e685d9eb7b622bcb4e9fb0273f030fc35971b
Batman2000 <nurmhit@mail.ru>
2019-12-07 18:48:19 +0300

    backend fixed

--- backend/src/main/java/com/example/demo4/WebSecurityConfig.java
+++ backend/src/main/java/com/example/demo4/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/id/1").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
