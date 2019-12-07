e5a477cd87e4da8c7d02e83ccd732fe3c6289227
karen efremjan <efremyan.kk@phystech.edu>
2019-12-07 20:48:32 +0300

    auth from front

--- frontend/src/components/Header/HeaderUserItems/index.js
+++ frontend/src/components/Header/HeaderUserItems/index.js
@@ -4,12 +4,11 @@ const HeaderUserItems = (props) => {
     let userNameDisplay = props.userName != null ? <strong style={{paddingRight:".2rem"}}>{props.userName}</strong> : "";
     return(
         <div className="app-user">
-            <a className="app-item" href='../login'>
+            <a className="app-item" href='http://localhost:8080/login'>
                 {userNameDisplay}
                 <img src={props.imgIcon} className="notification-icon" title={props.title} alt="user-item"/>
             </a>
         </div>        
     )
 }
-
 export default HeaderUserItems;
--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -10,7 +10,7 @@ import bagIcon from "../../images/bag-icon-2.png";
 import notificationIcon from "../../images/notification-icon3.png";
 import cartIcon from "../../images/cart-icon-6.png";
 import userIcon from "../../images/user-icon-1.png";
-
+import SERVER_URL from "../../../src/server/consts"
 import { Link } from 'react-router-dom';
 
 import './Header.css';
@@ -23,9 +23,7 @@ const Header = () => {
                 <HeaderSearch />
                 <div className="app-user-info">
                     <HeaderMenu />
-                    <a navlink key="1" to="/signin">
-                        <HeaderUserItems imgIcon={userIcon}  userName="Welcome!"/>
-                    </a>
+                    <HeaderUserItems imgIcon={userIcon}  userName="Welcome!"/>
                 </div>
             </div>
             <Navbar />
