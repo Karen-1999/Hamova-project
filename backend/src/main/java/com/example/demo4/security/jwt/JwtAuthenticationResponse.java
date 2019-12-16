58c86fc673234cb1636f6cc5e16f028a322aada3
Batman2000 <nurmhit@mail.ru>
2019-12-16 19:29:35 +0300

    constructor deleted

--- backend/src/main/java/com/example/demo4/security/jwt/JwtAuthenticationResponse.java
+++ backend/src/main/java/com/example/demo4/security/jwt/JwtAuthenticationResponse.java
@@ -1,16 +1,14 @@
 package com.example.demo4.security.jwt;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class JwtAuthenticationResponse {
     private String accessToken;
-    private String tokenType = "Bearer";
-
-    public JwtAuthenticationResponse(String accessToken) {
-        this.accessToken = accessToken;
-    }
+    final private String tokenType = "Bearer";
 }
